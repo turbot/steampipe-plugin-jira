576a3a9e8aeb52b3d7aa59fee331fed918e486e7
LalitTurbot <lalit@turbot.com>
2022-04-14 14:41:47 +0530

    Add optional quals for table jira_issue on fileds issue_id, issue_key, project_key, project_id, project_name, status, assignee_display_name, assignee_display_name, creator_account_id, creator_display_name, reporter_account_id, reporter_display_name, created, duedate, updated, epic_key, type and priority

--- jira/table_jira_sprint.go
+++ jira/table_jira_sprint.go
@@ -6,10 +6,10 @@ import (
 	"time"
 
 	"github.com/andygrunwald/go-jira"
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 )
 
 //// TABLE DEFINITION
