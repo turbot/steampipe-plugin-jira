6350dfa68d5177547c51f26e0940da2dfeece3d2
misraved <ved@turbot.com>
2022-09-07 03:17:48 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.6 and Go version 1.19

--- jira/table_jira_global_setting.go
+++ jira/table_jira_global_setting.go
@@ -3,9 +3,9 @@ package jira
 import (
 	"context"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 )
 
 //// TABLE DEFINITION
