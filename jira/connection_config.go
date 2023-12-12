9a44ca37f53fa02fd32f31c71558dd6e43745156
misraved <ved@turbot.com>
2023-12-12 14:21:50 +0530

    Update connection config variable definition to use the latest hcl syntax

--- jira/connection_config.go
+++ jira/connection_config.go
@@ -2,29 +2,13 @@ package jira
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type jiraConfig struct {
-	BaseUrl             *string `cty:"base_url"`
-	Username            *string `cty:"username"`
-	Token               *string `cty:"token"`
-	PersonalAccessToken *string `cty:"personal_access_token"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"base_url": {
-		Type: schema.TypeString,
-	},
-	"username": {
-		Type: schema.TypeString,
-	},
-	"token": {
-		Type: schema.TypeString,
-	},
-	"personal_access_token": {
-		Type: schema.TypeString,
-	},
+	BaseUrl             *string `hcl:"base_url"`
+	Username            *string `hcl:"username"`
+	Token               *string `hcl:"token"`
+	PersonalAccessToken *string `hcl:"personal_access_token"`
 }
 
 func ConfigInstance() interface{} {
