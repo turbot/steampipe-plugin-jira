07d067e8cdf73e02cd03551b7936644d1108d962
ParthaI <47887552+ParthaI@users.noreply.github.com>
2024-08-29 16:48:49 +0530

    Update `jira_issue` table to fetch 500 results per page (#133)

--- jira/table_jira_issue.go
+++ jira/table_jira_issue.go
@@ -246,9 +246,9 @@ func listIssues(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData)
 	// If the requested number of items is less than the paging max limit
 	// set the limit to that instead
 	queryLimit := d.QueryContext.Limit
-	var limit int = 50
+	var limit int = 500
 	if d.QueryContext.Limit != nil {
-		if *queryLimit < 50 {
+		if *queryLimit < 500 {
 			limit = int(*queryLimit)
 		}
 	}
